b2eab9ba13c7b0bd3993bbe6ef115ed85eff046b
sankardevops2020 <59198009+sankardevops2020@users.noreply.github.com>
2020-03-11 05:00:55 +0530

    Added the few date
    for test

--- sankar/src/main/java/hello/jar/App.java
+++ sankar/src/main/java/hello/jar/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello sankar!" );
+        System.out.println( "Hello ravi, are you looking for devops jobs?" );
     }
 }
